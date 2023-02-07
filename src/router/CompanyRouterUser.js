8793ade81eb4594af3756bd6ad1c97a12c0efd00
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-02-06 21:48:01 -0500

    cambios sebastian Gerardo 4.0

--- src/router/CompanyRouterUser.js
+++ src/router/CompanyRouterUser.js
@@ -5,15 +5,14 @@ import Perfil from "../pages/Perfil/Perfil";
 import Notifications from '../components/CompanyPage/Notifications/Notifications'
 import Logout from '../components/CompanyPage/Logout/Logout'
 
-export const Router = () => {
+export const Router2 = () => {
     return (
-        <BrowserRouter className='rts'>
-            <MenuEnterprise/>
+        <BrowserRouter>
             <Routes>
-                <Route path="/perfil" element={<Perfil/>}/>
-                <Route path="/developers" element={<Developers/>}/>        
-                <Route path="/notifications" element={<Notifications/>}/>        
-                <Route path="/logout" element={<Logout/>}/>        
+                    <Route path="/mn/perfil" element={<Perfil/>}/>
+                    <Route path="/mn/developers" element={<Developers/>}/>        
+                    <Route path="/mn/notifications" element={<Notifications/>}/>        
+                    <Route path="/mn/logout" element={<Logout/>}/>        
             </Routes>
         </BrowserRouter>
     )
