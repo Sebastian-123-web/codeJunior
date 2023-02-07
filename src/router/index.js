d5c4a56215e89d90502b276c23cbd669705e1ff8
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-02-06 19:54:56 -0500

    cambios 2.0 de Sebastian Gerardo

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Developers from "../components/Developers/Developers";
+import Developers from "../components/CompanyPage/Developers/Developers";
 import MenuEnterprise from '../components/MenuEnterprise/MenuEnterprise'
 import Perfil from "../pages/Perfil/Perfil";
-import Notifications from '../components/Notifications/Notifications'
-import Logout from '../components/Logout/Logout'
+import Notifications from '../components/CompanyPage/Notifications/Notifications'
+import Logout from '../components/CompanyPage/Logout/Logout'
 
 export const Router = () => {
     return (
