5ad43cffad609df5601d7017e1c77168ec33353a
Jorge Garcia <jorge.garcia.a@uni.pe>
2023-02-07 23:27:58 -0500

    Nuevos Cambios

--- src/router/CompanyRouterUser.js
+++ src/router/CompanyRouterUser.js
@@ -4,10 +4,16 @@ import MenuEnterprise from "../components/CompanyPage/MenuEnterprise/MenuEnterpr
 import Perfil from "../pages/Perfil/Perfil";
 import Notifications from '../components/CompanyPage/Notifications/Notifications'
 import Logout from '../components/CompanyPage/Logout/Logout'
+import Home from "../pages/Home/Home";
+import Login from "../pages/Home/Login";
 
 export const Router2 = () => {
     return (
         <BrowserRouter>
+            <Routes>
+                    <Route path="/" elemnt={<Home />}/>
+                    <Route path="/login" elemnt={<Login />}/>
+            </Routes>
             <Routes>
                     <Route path="/mn/perfil" element={<Perfil/>}/>
                     <Route path="/mn/developers" element={<Developers/>}/>        
