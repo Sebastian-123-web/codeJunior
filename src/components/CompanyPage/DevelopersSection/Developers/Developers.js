b48763f303bb84ca9ca43dc96b087ef3626cb1f6
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-02-08 01:58:18 -0500

    cambios pageDev modal

--- src/components/CompanyPage/DevelopersSection/Developers/Developers.js
+++ src/components/CompanyPage/DevelopersSection/Developers/Developers.js
@@ -1,78 +1,62 @@
 import React from 'react'
 import SearchDevelopers from '../SearchDevelopers/SearchDevelopers'
 import './Developers.css'
+import { useState, useEffect } from 'react'
+import { DataComDev } from '../../../../services/Company/DataComDevs'
+import Perfildeveloper from '../../../../pages/Developers/Perfil/Perfil'
 
 export default function Developers() {
+  const [ data, setData ] = useState([]);
+  const [ modal, setModal ] = useState(false);
+
+  const DataDevCom = async () => {
+    const response = await DataComDev();
+    setData(response)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    DataDevCom();
+  }, [])
+
+
   return (
     <section className='company-developers'>
       <SearchDevelopers/>
       <section className='company-dev-users'>
         <section className='company-dev-users_box'>
+          {data.length > 0 && data.map((event) => (
+            <div className='company-dev-users__cards' onClick={() => setModal(!modal)}>
 
-          <div className='company-dev-users__cards'>
-            
-            <div className='users-cards-header'>
-              <div className='users-cards-header__banner'>
-                <img src='https://timelinecovers.pro/facebook-cover/download/web-developer-coding-on-screen-hex2rgb-facebook-cover.jpg' alt=''/>  
+              <div className='users-cards-header'>
+                <div className='users-cards-header__banner'>
+                  <img src='https://timelinecovers.pro/facebook-cover/download/web-developer-coding-on-screen-hex2rgb-facebook-cover.jpg' alt=''/>  
+                </div>
+                <div className='users-cards-header__perfil'>
+                  <img src='https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1425034585/content-items/001/228/844/sesion-estudio-barcelona-10-original.jpg?1425034585' alt=''/>  
+                </div>
+              </div> 
+
+              <div className='users-cards-main'>
+                <h1>{event.nombre} {event.apellido}</h1>
+                  <div>{event.tecnologia.length > 0 && event.tecnologia.map((element) => (
+                    <img src={element.icon} alt='icon-tecnologies' className='icon-tc'/>
+                  ))}</div>  
               </div>
-              <div className='users-cards-header__perfil'>
-                <img src='https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1425034585/content-items/001/228/844/sesion-estudio-barcelona-10-original.jpg?1425034585' alt=''/>  
+
+              <div className='users-cards-footer'>
+                    
               </div>
-            </div> 
 
-            <div className='users-cards-main'>
-              <h1>Miguel Rodriguez</h1>
-                <p>Descriptionasdasdasdasdasdasdadasdasdadasdasasdasdasdasds</p>  
             </div>
+          ))}
 
-            <div className='users-cards-footer'>
-              <button>Ver más</button>
-            </div>
-
-          </div>
-          <div className='company-dev-users__cards'>
-            
-            <div className='users-cards-header'>
-              <div className='users-cards-header__banner'>
-                <img src='https://timelinecovers.pro/facebook-cover/download/web-developer-coding-on-screen-hex2rgb-facebook-cover.jpg' alt=''/>  
-              </div>
-              <div className='users-cards-header__perfil'>
-                <img src='https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1425034585/content-items/001/228/844/sesion-estudio-barcelona-10-original.jpg?1425034585' alt=''/>  
-              </div>
-            </div> 
-
-            <div className='users-cards-main'>
-              <h1>Miguel Rodriguez</h1>
-                <p>Descriptionasdasdasdasdasdasdadasdasdadasdasasdasdasdasds</p>  
-            </div>
-
-            <div className='users-cards-footer'>
-              <button>Ver más</button>
-            </div>
-
-          </div>
-          <div className='company-dev-users__cards'>
-            
-            <div className='users-cards-header'>
-              <div className='users-cards-header__banner'>
-                <img src='https://timelinecovers.pro/facebook-cover/download/web-developer-coding-on-screen-hex2rgb-facebook-cover.jpg' alt=''/>  
-              </div>
-              <div className='users-cards-header__perfil'>
-                <img src='https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1425034585/content-items/001/228/844/sesion-estudio-barcelona-10-original.jpg?1425034585' alt=''/>  
-              </div>
-            </div> 
-
-            <div className='users-cards-main'>
-              <h1>Miguel Rodriguez</h1>
-                <p>Descriptionasdasdasdasdasdasdadasdasdadasdasasdasdasdasds</p>  
-            </div>
-
-            <div className='users-cards-footer'>
-              <button>Ver más</button>
-            </div>
-
-          </div>
-
+        </section>
+      </section>
+      <section className={`company-dev-users-modal ${modal && 'activeCompany-dev-users-modal'}`} >
+        <div className={`background-modal ${modal && 'activeBackground-modal'}`} onClick={() => setModal(!modal)}></div>
+        <section className={`dev-users_modal ${modal && 'activeDev-users_modal'}`}>
+          <Perfildeveloper/>
         </section>
       </section>
     </section>
