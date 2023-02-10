14603ead0ac42e056d41e27a2a2bbf2e1e3ec627
Nando HT <whoole.lfht@gmail.com>
2023-02-10 15:52:06 -0500

    add regdev y regemp

--- src/pages/Registros/regdev/DevForm.js
+++ src/pages/Registros/regdev/DevForm.js
@@ -1,21 +1,33 @@
-import React, { useEffect, useState, useTransition } from 'react'
+import React, {  useState } from 'react'
 import "./DevForm.css";
 
 
 export default function DevForm() {
 
-  const [nombres,setNombres]= useState("");
-  const [apellidos, setApellidos] = useState("");
-  const [tipodoc, setTipodoc] = useState("");
-  const [numerodni, setNumerodni] = useState("");
-  const [numerocel, setNumerocel] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-const handleSubmit=e=>{
-  // e.preventDefault();
-  alert("El formulario se envio")
-}
+ const [formData, setFormData] = useState({
+   name: "",
+   surname: "",
+   documentType: "",
+   documentNumber: "",
+   phone: "",
+   email: "",
+   password: "",
+ });
+const [showPassword, setShowPassword] = useState(false);
+
+ const handleChange = (e) => {
+   setFormData({
+     ...formData,
+     [e.target.name]: e.target.value,
+   });
+ };
+
+ const handleSubmit = (e) => {
+  //  e.preventDefault();
+   // Aquí puedes enviar los datos del formulario a una API o base de datos
+   alert("El formulario se envio");
+ };
 
   return (
     <>
@@ -24,85 +36,95 @@ const handleSubmit=e=>{
           <h2> Formulario de registro</h2>
 
           <form className="devform" onSubmit={handleSubmit}>
-            <div className="a">
-              <label htmlFor="nombres">Nombres: </label>
-              <input
-                required
-                type="text"
-                id="nombres"
-                name="nombres"
-                value={nombres}
-                onChange={(e) => setNombres(e.target.value)}
-              />
-            </div>
-            <div className="a">
-              <label htmlFor="apellidos">Apellidos: </label>
-              <input
-                required
-                type="text"
-                id="apellidos"
-                name="apellidos"
-                value={apellidos}
-                onChange={(e) => setApellidos(e.target.value)}
-              />
-            </div>
-            <div className="a">
-              <label htmlFor="tipodoc">Tipo Documento: </label>
-              <select
-                name="tipodoc"
-                value={tipodoc}
-                onChange={(e) => setTipodoc(e.target.value)}
-              >
-                <option value="">-------</option>
-                <option value="dni">Dni</option>
-                <option value="pasaporte">Pasaporte</option>
-              </select>
+            <div className="left">
+              <div className="a">
+                <input
+                  placeholder="Nombres"
+                  required
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="tipoDeDoc a">
+                <select
+                  name="documentType"
+                  value={formData.documentType}
+                  onChange={handleChange}
+                >
+                  <option value="">T.doc</option>
+                  <option value="dni">Dni</option>
+                  <option value="pasaporte">Pasaporte</option>
+                </select>
 
-              <label htmlFor="numerodni">Numero: </label>
-              <input
-                required
-                type="number"
-                id="numerodni"
-                name="numerodni"
-                value={numerodni}
-                onChange={(e) => setNumerodni(e.target.value)}
-              />
+                <input
+                  className="number"
+                  placeholder="Numero"
+                  required
+                  type="number"
+                  id="numerodni"
+                  name="documentNumber"
+                  value={formData.documentNumber}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="a">
+                <input
+                  placeholder="E-mail"
+                  required
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
-            <div className="a">
-              <label htmlFor="numerocel">Celular: </label>
-              <input
-                required
-                type="number"
-                id="numerocel"
-                name="numerocel"
-                value={numerocel}
-                onChange={(e) => setNumerocel(e.target.value)}
-              />
+            <div className="right">
+              <div className="a">
+                <input
+                  placeholder="Apellidos"
+                  required
+                  type="text"
+                  id="apellidos"
+                  name="surname"
+                  value={formData.surname}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="a">
+                <input
+                  placeholder="Celular"
+                  required
+                  type="number"
+                  id="numerocel"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="a">
+                <input
+                  placeholder="Password"
+                  required
+                  type={showPassword ? "text" : "password"}
+                  id="password-input"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? "Ocultar" : "Mostrar"} contraseña
+                </button>
+              </div>
             </div>
-            <div className="a">
-              <label htmlFor="email">Email: </label>
-              <input
-                required
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="a">
-              <label htmlFor="password">Password: </label>
-              <input
-                required
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="a">
-              <input type="submit" />
+            <div className="boton ">
+              <button type="submit">Registrarse</button>
             </div>
           </form>
         </section>
