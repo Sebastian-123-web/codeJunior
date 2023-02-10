123d57dad516fc9db91fe66803a545aa7338b3b8
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-02-09 22:21:12 -0500

    add api

--- src/pages/Developers/PuertosTrabajos/PuestosTrabajo.jsx
+++ src/pages/Developers/PuertosTrabajos/PuestosTrabajo.jsx
@@ -1,11 +1,21 @@
 import './PuestosTrabajo.css'
+import puesto_trabajos_team from  '../../../assets/Developer/puesto_trabajos-team.svg';
+import { useState } from 'react';
+import { DataComDev } from '../../../services/Company/DataComDevs';
 
 const PuestosTrabajos = () => {
+
+    const [ business, setBusiness ] = useState({});
+
+    const fetchBusiness = async() => {
+        const dataBusiness = DataComDev('business');
+    }
+
     return(
         <div className="puestos_trabajos">
             <div className="banner">
-                <img src="" alt="" />
-                <h1>Encuentra el empleo que se acomode mas a tus grandes habilidades</h1>
+                <img src={puesto_trabajos_team} alt="" className='banner-img' />
+                <h1 className='banner-50'>Encuentra el empleo que se acomode mas a tus grandes habilidades</h1>
             </div>
             <div className="search_job">
                 <input type="text" className='search_job_-input' placeholder='Buscar Empleo' />
