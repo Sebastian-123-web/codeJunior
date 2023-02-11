1ec189c62b3350ac29eda92de6efe43eed1e01fc
Jorge Garcia <jorge.garcia.a@uni.pe>
2023-02-10 21:32:37 -0500

    cambios Jorge 3.0

--- src/components/Home/MainHome/MainHome.jsx
+++ src/components/Home/MainHome/MainHome.jsx
@@ -93,15 +93,43 @@ const images = [
   export default function ButtonBases() {
     return (
       <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
-        {images.map((image) => (
+        
           <ImageButton
             focusRipple
-            key={image.title}
+            key={images[0].title}
             style={{
-              width: image.width,
+              width: images[0].width,
             }}
           >
-            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+            <ImageSrc style={{ backgroundImage: `url(${images[0].url})` }} />
+            <ImageBackdrop className="MuiImageBackdrop-root" />
+            <Image>
+              <Link className="link-buttons" to="/formEmp">
+                <Typography
+                    component="span"
+                    variant="subtitle1"
+                    color="inherit"
+                    sx={{
+                    position: 'relative',
+                    p: 4,
+                    pt: 2,
+                    pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                    }}
+                >
+                    {images[0].title}
+                    <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+              </Link>  
+            </Image>
+          </ImageButton>
+          <ImageButton
+            focusRipple
+            key={images[1].title}
+            style={{
+              width: images[1].width,
+            }}
+          >
+            <ImageSrc style={{ backgroundImage: `url(${images[1].url})` }} />
             <ImageBackdrop className="MuiImageBackdrop-root" />
             <Image>
               <Link className="link-buttons" to="/formDev">
@@ -116,14 +144,13 @@ const images = [
                     pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
                     }}
                 >
-                    {image.title}
+                    {images[1].title}
                     <ImageMarked className="MuiImageMarked-root" />
                 </Typography>
               </Link>  
-              
             </Image>
           </ImageButton>
-        ))}
+        
       </Box>
     );
   }
@@ -158,7 +185,27 @@ const MainHome = () => {
                         <p className="content-text">Si buscas iniciar tu primera experiencia laboral en el desarrollo de software o cuentas con una experiencia menor a 2 años, podras conectarte con reconocidas empresas del rubro con la que podras entrar en contacto.</p>
                     </div>
                 </section>
+                <section className="home-about-us">
+                    <h2 className="about-title">Acerca de</h2>
+                    <p className="about-text">Somos una joven empresa Peruana que busca potenciar la industria tecnologica, desafiando la actual manera de contratacion de talentos tecnologicos.</p>
+                    <p className="about-text">En tan solo 5 dias entregamos una terna de perfiles para que se incorporen a su equipo, y por los 3 primeros meses, este desarrollador estara en prueba.</p>
+                    <p className="about-text">Nos importa que las cosas pasen con una mirada centrada en resultados, mas que en horarios y lugares fisicos, por lo que apostamos por trabajar 100% remoto y asi tambien, incentivar a nuestros clientes, a que sus contrataciones sigan en la misma linea.</p>
+                </section>
+               
                 <section className="testimonials">
+                  <div className="home-diferenciales">
+                      <h2 className="diferenciales-title">Diferenciales</h2>
+                      <div className="home-list-1">
+                        <ul className="diferenciales-list">
+                          <li className="diferenciales-item">Modalidad de contratacion 100% remoto.</li>
+                          <li className="diferenciales-item">Perfiles seleccionados de una base de datos.</li>
+                          <li className="diferenciales-item">Oportunidad de conseguir tu primera experiencia laboral.</li>
+                          <li className="diferenciales-item">Seguimiento y asesoria a los desarrolladores.</li>
+                          <li className="diferenciales-item">Contratacion de personal en todos los paises disponibles.</li>
+                        </ul>
+                      </div>
+                  </div>
+                  <div className="testimonials-home-container">
                     <h2>Testimonios</h2>
                     <p>¿Que dicen de nosotros?</p>
                     <div className="card-containers">
@@ -224,6 +271,22 @@ const MainHome = () => {
                                         </div>
                                      </div>   */}
                         
+                    </div>
+                  </div>
+                </section>
+                <section className="home-contacto">
+                    <h2>Contacto</h2>
+                    <div className="contact-wrapper">
+                        
+                        <div className="map-container">
+                            <iframe className="iframe-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63390.89404368884!2d-79.86765562304934!3d-6.778267495100513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x904cef2c56f229ad%3A0x9ef53152a0c6b9d!2sReal%20Plaza%20Chiclayo.!5e0!3m2!1ses!2spe!4v1676073447675!5m2!1ses!2spe" width="700" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
+                        <div className="contact-container">
+                              <p><strong>Direccion:</strong> Cale Algun Lugar XYZ, Chiclayo</p>
+                              <p><strong>Telefono:</strong> 01 - 555 4444</p>
+                              <p><strong>email: </strong> contacto@devjuniors.com</p>
+                        </div>
                     </div>
                 </section>
             </main>
